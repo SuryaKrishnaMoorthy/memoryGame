61e2953a6cc0db01c2830f1fddc11570cb24c756
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2018-06-04 12:14:57 -0700

    write a function to populate the board with cat images and hide them after a second

--- bundle.js
+++ bundle.js
@@ -1,5 +1,6 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-let images = ["100.jpg", "101.jpg", "200.jpg", "201.jpg", "202.jpg", "204.jpg", "206.jpg", "207.jpg", "207.jpg", "207.jpg"];
+let images = require("./scripts/data");
+// let images = ["100.jpg", "101.jpg", "200.jpg", "201.jpg", "202.jpg", "204.jpg", "206.jpg", "207.jpg", "207.jpg", "207.jpg"];
 
 let levelButton = Array.from(document.querySelectorAll(".button-style"));
 let gameWindow = document.querySelector(".game-window");
@@ -20,30 +21,32 @@ levelButton.forEach(button => {
         break;
     }
     gameWindow.style.display = "none";
+    populateBoard(duplicatedImages);
+
+    // console.log(populateBoard(duplicatedImages))
+    // let cards = Array.from(document.querySelectorAll(".card"));
   })
 })
+//function to hide the cat images from game board
+function hideCats(){
+  let catImage = Array.from(document.querySelectorAll(".catImage"));
+  catImage.forEach((cat) => {
+    cat.classList.add("cardImage");
+  })
+}
 
+//create the cards in the div and set the default image when card in faced down
 function createBoard(levelNumber) {
   for (let i = 0; i < levelNumber; i++) {
     let gameBoard = document.querySelector(".gameBoard");
     let card = document.createElement("div");
     card.classList.add("card");
-    card.innerHTML = `<img   src="http://sci.esa.int/science-e-media/img/7d/heic1211a_screen.jpg" width="150" height="150">`
+    card.style.backgroundImage = `url("http://sci.esa.int/science-e-media/img/7d/heic1211a_screen.jpg")`;
+    // console.log(card);
     gameBoard.appendChild(card);
   }
 }
 createBoard(16);
-//
-// function randomizeArray(images, levelNumber){
-//   let randomizedArray = [];
-//   for(let i = 0 ;i < levelNumber/2; i++){
-//     let index = Math.floor(Math.random()* images.length);
-//     randomizedArray.push(images[index]);
-//   }
-//   return randomizedArray;
-// }
-//
-const duplicatedImages = images.push(...images);
 
 //shuffle the images
 function shuffleArray(images) {
@@ -56,12 +59,27 @@ function shuffleArray(images) {
   }
   return images;
 }
-console.log(shuffleArray(images));
 
+//create an array with images duplicated
+const duplicatedImages = [...images, ...images];
+// console.log(duplicatedImages);
 
-function populateBoard() {
-  let cards = Array.from(document.querySelectorAll(".card"));
-  cards
+//set the images of the card as cats.
+let cards = Array.from(document.querySelectorAll(".card"));
+
+function populateBoard(duplicatedImages) {
+  let shuffledArray = shuffleArray(duplicatedImages);
+  cards.forEach((card,index) => {
+    let imagePath = `./assets/${shuffledArray[index]}`;
+    card.innerHTML = `<img class="catImage"  src="${imagePath}" width="150" height="150">`;
+  })
+  //hides the cat images after a second
+  setTimeout(hideCats, 1000);
+  gameWindow.style.display = "none";
 }
+
+},{"./scripts/data":2}],2:[function(require,module,exports){
+let images = ["100.jpg", "101.jpg", "200.jpg", "201.jpg", "202.jpg", "204.jpg", "206.jpg", "207.jpg"];
+module.exports = images;
 
 },{}]},{},[1]);
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let images = ["100.jpg", "101.jpg", "200.jpg", "201.jpg", "202.jpg", "204.jpg", "206.jpg", "207.jpg", "207.jpg", "207.jpg"];
+let images = require("./scripts/data");
+// let images = ["100.jpg", "101.jpg", "200.jpg", "201.jpg", "202.jpg", "204.jpg", "206.jpg", "207.jpg", "207.jpg", "207.jpg"];
 
 let levelButton = Array.from(document.querySelectorAll(".button-style"));
 let gameWindow = document.querySelector(".game-window");
@@ -19,30 +20,32 @@ levelButton.forEach(button => {
         break;
     }
     gameWindow.style.display = "none";
+    populateBoard(duplicatedImages);
+
+    // console.log(populateBoard(duplicatedImages))
+    // let cards = Array.from(document.querySelectorAll(".card"));
   })
 })
+//function to hide the cat images from game board
+function hideCats(){
+  let catImage = Array.from(document.querySelectorAll(".catImage"));
+  catImage.forEach((cat) => {
+    cat.classList.add("cardImage");
+  })
+}
 
+//create the cards in the div and set the default image when card in faced down
 function createBoard(levelNumber) {
   for (let i = 0; i < levelNumber; i++) {
     let gameBoard = document.querySelector(".gameBoard");
     let card = document.createElement("div");
     card.classList.add("card");
-    card.innerHTML = `<img   src="http://sci.esa.int/science-e-media/img/7d/heic1211a_screen.jpg" width="150" height="150">`
+    card.style.backgroundImage = `url("http://sci.esa.int/science-e-media/img/7d/heic1211a_screen.jpg")`;
+    // console.log(card);
     gameBoard.appendChild(card);
   }
 }
 createBoard(16);
-//
-// function randomizeArray(images, levelNumber){
-//   let randomizedArray = [];
-//   for(let i = 0 ;i < levelNumber/2; i++){
-//     let index = Math.floor(Math.random()* images.length);
-//     randomizedArray.push(images[index]);
-//   }
-//   return randomizedArray;
-// }
-//
-const duplicatedImages = images.push(...images);
 
 //shuffle the images
 function shuffleArray(images) {
@@ -55,10 +58,21 @@ function shuffleArray(images) {
   }
   return images;
 }
-console.log(shuffleArray(images));
 
+//create an array with images duplicated
+const duplicatedImages = [...images, ...images];
+// console.log(duplicatedImages);
 
-function populateBoard() {
-  let cards = Array.from(document.querySelectorAll(".card"));
-  cards
+//set the images of the card as cats.
+let cards = Array.from(document.querySelectorAll(".card"));
+
+function populateBoard(duplicatedImages) {
+  let shuffledArray = shuffleArray(duplicatedImages);
+  cards.forEach((card,index) => {
+    let imagePath = `./assets/${shuffledArray[index]}`;
+    card.innerHTML = `<img class="catImage"  src="${imagePath}" width="150" height="150">`;
+  })
+  //hides the cat images after a second
+  setTimeout(hideCats, 1000);
+  gameWindow.style.display = "none";
 }
